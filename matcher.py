ee7c1ecd0e5475774d4d6bc86efe890ca68135f4
DURGESHRAMANA <161280911+DURGESHRAMANA@users.noreply.github.com>
2025-08-04 12:22:18 +0530

    Updated matcher.py

--- matcher.py
+++ matcher.py
@@ -5,7 +5,6 @@
 import os
 import re
 import pdfplumber
-import fitz
 import docx
 import easyocr
 import pandas as pd
@@ -28,22 +27,13 @@ def extract_text_from_file(file_path):
     ext = os.path.splitext(file_path)[1].lower()
 
     if ext == '.pdf':
-        try:
-            text = ""
-            with pdfplumber.open(file_path) as pdf:
-                for page in pdf.pages:
-                    page_text = page.extract_text()
-                    if page_text:
-                        text += page_text + "\n"
-            if len(text.strip()) < 100:
-                raise ValueError("pdfplumber returned too little text.")
-        except:
-            text = ""
-            doc = fitz.open(file_path)
-            for page in doc:
-                text += page.get_text()
+        text = ""
+        with pdfplumber.open(file_path) as pdf:
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + "\n"
         return text
-
     elif ext == '.docx':
         doc = docx.Document(file_path)
         return "\n".join([para.text for para in doc.paragraphs])
